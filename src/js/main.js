5a2d7e0ddb45207f949d050b9d37229b2ec8c753
Андрей Гордиенко <andrejgordienko@Andrejs-MacBook-Air.local>
2021-05-16 17:36:23 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+// without JQEURY
+// window.addEventListener('DOMContentLoaded', () => {
+//     'use sctrict'
+
+//     const modal = (selector, openDelay) => {
+//         const modal = document.querySelector(selector);
+//         const close = modal.querySelector('[data-close]');
+//         const content = modal.querySelector('[data-modal]');
+
+//         close.addEventListener('click', () => {
+//             modal.style.display = 'none';
+//         })
+
+//         const openModalByTime = () => {
+//             if (!sessionStorage.getItem('modal-opened')) {
+//                 setTimeout(() => {
+//                     modal.style.display = 'flex';
+//                     setTimeout(() => {
+//                         content.style.opacity = '1';
+//                     }, 100);
+//                 }, openDelay);
+//                 sessionStorage.setItem('modal-opened', 'true');
+//             }
+//         }
+//         openModalByTime();
+//     }
+
+//     modal('.modal', 6000);
+// })
+$(function(){
+    'use strict'
+
+    const modal = (selector, time) => {
+        const openModalByTime = () => {
+            if (!sessionStorage.getItem('modal-opened')) {
+                setTimeout(() => {
+                    $(selector).fadeIn('slow')
+                    $(selector).css('display', 'flex')
+                }, time);
+                sessionStorage.setItem('modal-opened', 'true');
+            }
+        }
+        openModalByTime();
+
+        $('.modal__close').click(() => {
+            $('.modal').fadeOut()
+        })
+    }
+    modal('.modal', 5000)
+})
